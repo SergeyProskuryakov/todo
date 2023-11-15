d37e904cd78ea8f0647b9479ac65c985fd1c02cc
SergeyProskuryakov <130496878+SergeyProskuryakov@users.noreply.github.com>
2023-11-15 16:50:58 +0300

    таблица в админке

--- todo/mainpage/admin.py
+++ todo/mainpage/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Tasks
 
 # Register your models here.
+class TasksAdmin(admin.ModelAdmin):
+    list_display = [
+        field.name for field in Tasks._meta.fields if field.name != 'id'
+    ]
+admin.site.register(Tasks, TasksAdmin)
--- todo/mainpage/models.py
+++ todo/mainpage/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+# В этом файле создаём классы, описывающие структуры необходимых приложению таблиц
+
+class Tasks(models.Model):
+    # id создастся сам
+    start = models.DateTimeField(default=datetime.now)
+    end = models.DateField()
+    description = models.CharField(max_length=256, default='')
+    done = models.BooleanField(default=False)
